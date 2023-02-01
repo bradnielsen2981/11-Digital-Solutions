60bcf76fda6b00c6eab18a0874dbaaf016313d6d
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-02-01 12:02:49 +1000

    Commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
+/*
 cost = 1.5; //float or decimal
 message = "Your total cost is: "; //string
 shopclosed = false; //boolean
@@ -8,4 +9,11 @@ numberofapples = Number(numberofapples);
 total = cost * numberofapples;
 total = String(total);
 alert(message + total);
+*/
+
+//              0     1       2
+applearray = ['red','green','yellow']; //list of string
+
+console.log(applearray[0]);
+console.log(applearray.length);
 
