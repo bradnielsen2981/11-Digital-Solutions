65c5566eaa0cadfa7fb0a39a17f166ffba0f5bb1
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-02-01 14:16:51 +1000

    commit

--- js/myscript.js
+++ js/myscript.js
@@ -10,18 +10,28 @@ total = cost * numberofapples;
 total = String(total);
 alert(message + total);
 */
-
 //              0     1       2
 applearray = ['red','green','yellow', 45]; //list of string
 for (i=0; i < applearray.length; i++)
 {
     console.log(applearray[i])
 }
-
 //JavaScript object
 car = { type:"Fiat", model:"500", color:"white" };
 console.log(car.type);
 car.type = "Lamborgini";
 console.log(car.type);
+
+answer = document.getElementById("answer");
+answer.innerHTML = "A web server is a computer program that delivers HTML";
+
+function hideimage()
+{
+    image = document.getElementById("myimage");
+    image.style.display = "None";
+}
+
+image = document.getElementById("myimage");
+image.onmouseover = hideimage;
 
 
