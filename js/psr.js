1bf3549245b8be12e8d21a1fd08b4d4672476151
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-02-02 10:19:31 +1000

    commit

--- js/psr.js
+++ js/psr.js
@@ -29,14 +29,18 @@ function logic(playermove,computermove)
     }
 }
 
-//trigger by event
+    //new game
     computermove = getRandomItem(movearray); //Computer chooses a random move
     output = document.getElementById("output");
     output.innerHTML = "What is your move? ";
+
+    //player clicked button
     response = document.getElementById("input").value;
     //comparison of the player move to the computer move
     playerwin = logic(playermove,computermove);
     alert(playerwin);
+
+    
     //if playerwins add 1 to score else if computer wins add 1 to computer score else
     output.innerHTML = "Do you wish to play again ?"; 
     response = document.getElementById("input").value;
