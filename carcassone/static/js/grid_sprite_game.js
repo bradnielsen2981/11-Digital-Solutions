4a08613c1b38204ad55c76d94544ab3e9543b014
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-02-28 12:07:52 +1000

    commit

--- carcassone/static/js/grid_sprite_game.js
+++ carcassone/static/js/grid_sprite_game.js
@@ -1,21 +1,12 @@
+piece = "";
 
-//great a list of grid_sprites
-function create_sprites() {
-  //Create all the sprites
-  for (i=0; i<10; i++)
-  {
-      let column = Math.floor(Math.random()*GAME.GRID[0].length);
-      let row = Math.floor(Math.random()*GAME.GRID.length);
-
-      while (GAME.GRID[row][column] != 0)
-      {
-          column = Math.floor(Math.random()*GAME.GRID[0].length);
-          row = Math.floor(Math.random()*GAME.GRID.length);
-      }
-      
-      mySprite = new Grid_Sprite(column,row,"static/images/pig.png", 1); //Create a new Sprite
-  } 
-}
+monastry.onclick = function() {
+  piece = monastry;
+  p = new Grid_Sprite(0,0,'static/images/monastry.png',"M");
+  SPRITE_LIST.push(p);
+  GRID[0][0] = "M";
+  console.log(GRID);
+};
 
 // Called every frame
 function update_game()
