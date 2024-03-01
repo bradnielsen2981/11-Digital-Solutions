e3e78a2cf53a796adf20d06dcaf2a5168242f691
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-03-01 13:05:31 +1000

    commit

--- zombiechase_template/static/js/ball_sprite.js
+++ zombiechase_template/static/js/ball_sprite.js
@@ -1,6 +1,6 @@
 // A CLASS is a variable type that contains OTHER variables and functions.
 // WHEN IT IS CREATED, ITS CALLED AN OBJECT!!!)
-class Hero_Sprite
+class Ball_Sprite
 {
     constructor(x, y, width, height, image) { //a constructor is the function called when the OBJECT is created
       this.x = x;
@@ -12,8 +12,8 @@ class Hero_Sprite
       this.hspeed = 0;
       this.vspeed = 0;
       this.angle = 0;
-      this.speed = 2;
-      this.margin = -2; //the margin for detecting collision - collision mask has not yet been implemented
+      this.speed = 3;
+      this.margin = 0; //the margin for detecting collision - collision mask has not yet been implemented
       GAME.SPRITE_LIST.push(this); //add to game sprite list
     }
 
@@ -29,8 +29,6 @@ class Hero_Sprite
     //do any logic - called every frame
     update()
     {
-      //this.angle = this.angle + 0.05;
-
       this.x += this.hspeed*this.speed;
       this.y += this.vspeed*this.speed;
       
@@ -38,75 +36,14 @@ class Hero_Sprite
       let othersprite = GAME.sprite_collision_with_spritelist(this, GAME.SPRITE_LIST);
       if (othersprite != null)
       {
-        
+        console.log("Collision between sprites was detected.")
         //GAME.SPRITELIST.remove(this); //remove the sprite from the list of sprites being drawn, rendered
         //GAME.SPRITELIST.remove(othersprite);
+        //GAME.reflect_sprite_off_sprite(this, othersprite);
       }
-    }
 
-    //on mouse down
-    on_mouse_down(x,y) //on mouse down
-    {
-
-    }
-
-    //on mouse move
-    on_mouse_move(x,y) //on moving the mouse
-    {
-    }
-
-    //on mouse up
-    on_mouse_up(x,y)
-    {
-      if (GAME.sprite_collision_with_point(this, x, y))  //if mouse up on sprite
-      {         
-      }
-    }
-
-    //on key down
-    on_key_down(keycode, letter, keyspressed) //on key down
-    {
-      console.log(keyspressed);
-      // Process keys that are currently pressed
-      if (keyspressed[' ']) {
-        console.log("Space was pressed");
-
-        let bullet = new Ball_Sprite(this.x,this.y,10,10,"static/images/ball.png");
-        GAME.set_direction_of_sprite_to_point(bullet,GAME.MOUSEX,GAME.MOUSEY)
-
-      } else {
-        if (keyspressed['A'])
-        {
-          if (this.hspeed > -2)
-          {
-            this.hspeed -= this.speed;
-          }
-        }
-        if (keyspressed['D'])
-        {
-          if (this.hspeed < 2)
-          {
-            this.hspeed += this.speed;
-          }
-        }
-
-        if (keyspressed['W'])
-        {
-            this.y -= this.speed;
-        }
-        if (keyspressed['S'])
-        {
-            this.y += this.speed;
-        }
-
-        
-      }
-    }
-
-    on_key_up(keycode, letter) //on key up    {
-    {
-
-    }    
+      //GAME.reflect_sprite_off_boundary(this, GAME.CANVAS);
+    }  
 
     destroy()
     {
